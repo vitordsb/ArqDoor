ce0051682af3e002d7647adab3c29593a703a154
vitordsb <vitordsb2019@gmail.com>
2025-11-23 13:11:28 -0300

    Deploy automático

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,6 +1,6 @@
 import { QueryClient } from "@tanstack/react-query";
 // export const API_BASE_URL = "http://localhost:8080";
-export const API_BASE_URL = "https://arqdoor.com";
+export const API_BASE_URL = "https://arqdoor.com/api";
 
 export async function apiRequest(
   method: string,
