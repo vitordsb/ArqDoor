6f0e30f3f7fb2b1a7bcc6ce56e7ed257277464ec
vitordsb <vitordsb2019@gmail.com>
2025-11-23 01:37:36 -0300

    Deploy automático

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,5 +1,5 @@
 import { QueryClient } from "@tanstack/react-query";
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "http://89.116.225.129:8080";
 
 export async function apiRequest(
   method: string,
